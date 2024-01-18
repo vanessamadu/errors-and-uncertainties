aa787ff4ee6911caf71462853c9338a3f77e7444
Vanessa Madu <vm2218@ic.ac.uk>
2024-01-18 14:07:51 +0000

    corrections

--- error_class.py
+++ error_class.py
@@ -1,28 +1,23 @@
 # general error class
-
-class Error:
-    def __init__(self,error:float,uncertainty:float,error_type:str,uncertainty_type:str):
-        self._error_type = error_type
-        self._error = error
-        self._uncertainty = uncertainty
-        self._uncertainty_type = uncertainty_type
-
-    @property
+from abc import ABC,abstractproperty
+class Error(ABC):
+    
+    @abstractproperty
     def error(self):
-        return self._error
+        pass
     
     @property
     def uncertainty(self):
-        return self._uncertainty
+        pass
     
     @property
     def error_type(self):
-        return self._error_type
+        pass
     
     @property
     def uncertainty_type(self):
-        return self._uncertainty_type
+        pass
     
     @property
     def error_summary(self):
-        return {self.error_type:self.error, self.uncertainty_type:self.uncertainty}
+        pass
