1c9e08f7ab3ea2e7f94bec28f19ae11c22d0d9b3
Vanessa Madu <vm2218@ic.ac.uk>
2024-01-18 13:33:35 +0000

    error class shell

--- error-class.py
+++ error-class.py
@@ -1,1 +1,13 @@
 # general error class
+class Error:
+    def __init__(self,predictions,observations):
+        self._predictions = predictions
+        self._observations = observations
+    
+    @property
+    def predictions(self):
+        return self._predictions
+    
+    @property
+    def observations(self):
+        return self._observations
