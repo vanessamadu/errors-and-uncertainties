68d6d3041fceff88346d2a815753dffd670a1d54
Vanessa Madu <vm2218@ic.ac.uk>
2024-01-19 14:24:53 +0000

    basic shell

--- maao_class.py
+++ maao_class.py
@@ -1,2 +1,15 @@
 from error_class import Error
 import numpy as np
+
+class MAAO(Error):
+    def __init__(self,predictions,observations):
+        self._predictions = predictions
+        self._observations = observations
+
+    @property
+    def predictions(self):
+        return self._predictions
+    
+    @property
+    def observations(self):
+        return self._observations
