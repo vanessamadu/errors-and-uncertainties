427b92042d64f780809b221b152d0b198dc5aa2b
Vanessa Madu <vm2218@ic.ac.uk>
2024-01-19 14:54:13 +0000

    residuals

--- maao_class.py
+++ maao_class.py
@@ -1,10 +1,11 @@
 from error_class import Error
 import numpy as np
+from numpy import linalg
 
 class MAAO(Error):
     def __init__(self,predictions,observations):
-        self._predictions = predictions
-        self._observations = observations
+        self._predictions = np.array(predictions)
+        self._observations = np.array(observations)
 
     @property
     def predictions(self):
@@ -13,3 +14,9 @@ class MAAO(Error):
     @property
     def observations(self):
         return self._observations
+    
+    @property
+    def residuals(self):
+        'cosine of the angle between each prediction and observation'
+        return np.array([np.dot(pred,obs)/(linalg.norm(pred)*linalg.norm(obs)) 
+                            for pred,obs in zip(self.predictions,self.observations)])
