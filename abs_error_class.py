4f2f46133fa9dd13a836da687f1f952fa3652596
Vanessa Madu <vm2218@ic.ac.uk>
2024-01-19 14:06:56 +0000

    abs-error-class

--- abs_error_class.py
+++ abs_error_class.py
@@ -1,4 +1,5 @@
 from error_class import Error
+import numpy as np
 
 class MAE(Error):
     def __init__(self,predictions,observations):
@@ -12,3 +13,25 @@ class MAE(Error):
     @property
     def observations(self):
         return self._observations
+    
+    @staticmethod
+    def mae(residuals_arr):
+        return np.mean(np.abs(residuals_arr))
+    
+    ## Error-inherited properties
+
+    @property
+    def error_type(self):
+        return "mae"
+    
+    @property
+    def error(self):
+        return __class__.mae(self.residuals)
+    
+    @property
+    def uncertainty_type(self):
+        return "std"
+    
+    @property
+    def uncertainty(self):
+        return np.std(self.residuals)
