71d28995f61be9096ba53259b685c84d51121e9e
Vanessa Madu <vm2218@ic.ac.uk>
2024-01-20 13:14:52 +0000

    add err summary

--- sems_class.py
+++ sems_class.py
@@ -19,15 +19,15 @@ class SEMs:
 
     @property
     def over_estimate_speed_indices(self):
-        return [i for i in range(len(self.residuals)) if self.residuals[i] > 0]
+        return np.array([i for i in range(len(self.residuals)) if self.residuals[i] > 0])
 
     @property
     def under_estimate_speed_indices(self):
-        return [i for i in range(len(self.residuals)) if self.residuals[i] < 0]
+        return np.array([i for i in range(len(self.residuals)) if self.residuals[i] < 0])
     
     @property
     def correct_estimate_speed_indices(self):
-        return [i for i in range(len(self.residuals)) if self.residuals[i] == 0]
+        return np.array([i for i in range(len(self.residuals)) if self.residuals[i] == 0])
     
     # speed error metrics
 
@@ -37,12 +37,34 @@ class SEMs:
     
     @property
     def over_under_correct_proportions(self):
-        return [len(part)/len(self.predictions) for part in 
+        return np.array([len(part)/len(self.predictions) for part in 
                 zip(self.over_estimate_speed_indices,
                     self.under_estimate_speed_indices,
-                    self.correct_estimate_speed_indices)]
+                    self.correct_estimate_speed_indices)])
 
+    @property
+    def ma_overestimated_e(self):
+        return MAE(self.predictions[self.over_estimate_speed_indices],
+                   self.observations[self.over_estimate_speed_indices])
     
+    @property
+    def ma_underestimated_e(self):
+        return MAE(self.predictions[self.under_estimate_speed_indices],
+                   self.observations[self.under_estimate_speed_indices])
+
+    ## Error-inherited property overwrites
+    @property
+    def error_summary(self):
+        err_metrics = [self.mae_speed,self.ma_overestimated_e,self.ma_underestimated_e]
+        err_metric_names = ["MAE over all Speeds","MAE for Overestimated Speeds", "MAE for Underestimated Speeds"]
+        err_summary = {}
+        for ii in range(len(err_metrics)):
+            err_summary[f"{err_metric_names[ii]} Error: {err_metrics[ii].error_type}":err_metrics[ii].error]
+            err_summary[f"{err_metric_names[ii]} Uncertainty: {err_metrics[ii].uncertainty_type}"
+                        :err_metrics[ii].uncertainty]
+        err_summary["Proportion of Over/Under/Correct Estimates of Speed":f"{self.over_under_correct_proportions*100}%"]
+        return err_summary
+
 
     
     
