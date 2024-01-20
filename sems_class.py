314f58a0a3e6788051a94827099ba3563f70c455
Vanessa Madu <vm2218@ic.ac.uk>
2024-01-20 12:42:13 +0000

    partition indices

--- sems_class.py
+++ sems_class.py
@@ -1,5 +1,41 @@
 # Speed Error Metrics Class
 from mae_class import MAE
+import numpy as np
 
-class SEMs(MAE):
-    pass
+class SEMs:
+    def __init__(self,predictions,observations):
+        self._predictions = np.array(predictions)
+        self._observations = np.array(observations)
+
+    @property
+    def predictions(self):
+        return self._predictions
+    
+    @property
+    def observations(self):
+        return self._observations
+    
+    ## split up into over estimated speed, under estimated speed, and correct speed
+
+    @property
+    def over_estimate_speed_indices(self):
+        return [i for i in range(len(self.residuals)) if self.residuals[i] > 0]
+
+    @property
+    def under_estimate_speed_indices(self):
+        return [i for i in range(len(self.residuals)) if self.residuals[i] < 0]
+    
+    @property
+    def correct_estimate_speed_indices(self):
+        return [i for i in range(len(self.residuals)) if self.residuals[i] == 0]
+    
+    # speed error metrics
+
+    @property
+    def mae_speed(self):
+        return MAE(self.predictions,self.observations)
+    
+
+
+    
+    
