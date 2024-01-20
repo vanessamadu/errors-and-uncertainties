0fbcfcf1952d5b6ab8d53183282ff18ce5096209
Vanessa Madu <vm2218@ic.ac.uk>
2024-01-20 14:03:10 +0000

    basic shell

--- aoems_class.py
+++ aoems_class.py
@@ -1,1 +1,17 @@
 # Angle Offset Error Metrics
+from maao_class import MAAO
+import numpy as np
+import pandas as pd
+
+class AOEMS:
+    def __init__(self,predictions,observations):
+        self._predictions = np.array(predictions)
+        self._observations = np.array(observations)
+    
+    @property
+    def predictions(self):
+        return self._predictions
+    
+    @property
+    def observations(self):
+        return self._observations
