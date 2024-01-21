5016b8652373e2bf4a8239636ab4303da1ab3f5f
Vanessa Madu <vm2218@ic.ac.uk>
2024-01-21 15:45:48 +0000

    simplify

--- drifter_velocity_models/model_classes.py
+++ drifter_velocity_models/model_classes.py
@@ -17,26 +17,8 @@ class Model:
     this class will be the parent class of all ocean models that we will be using
     and it will define attributes and methods common to all specific model classes.
     '''
-    def __init__(self,training_data:List[float],test_data:List[float]):
-        ## model specifiers
-        self.model_type = None
-        ## data attributes
-        self.training_data = training_data # data as pd dataframe
-        self.test_data = test_data # data as pd dataframe
-        ## for probabilistic regression models
-        self.trained_distribution = None
-        self.test_distribution = None
-
-    #++++++++++++++++++++++++ STATIC METHODS ++++++++++++++++++++++++++++#
-    @staticmethod
-    def to_degrees(arr:List[float]):
-        return np.rad2deg(arr)
-    
-    @staticmethod
-    def to_cm_per_second(arr:List[float]):
-        return np.multiply(100,arr)
-    
-    # -------------------- validation -------------------- #
+    def __init__(self,data):
+        self._data = data
 
     @staticmethod
     def check_coordinates(lon:float,lat:float):
@@ -51,27 +33,10 @@ class Model:
             finally:
                 if np.abs(values[coord])>limits[coord]:
                     raise ValueError(f"{coord} must be between -{limits[coord]} and {limits[coord]}")
-
-    #++++++++++++++++++++++ MODEL PROPERTIES AND SETTERS +++++++++++++++++++++#
-    # -------------------- properties ---------------------#
+                
     @property
-    def training_data(self):
-        '(setter) data used to train'
-        return self._training_data
-    
-    @property
-    def test_data(self):
-        '(setter) data used to test'
-        return self._test_data
-    
-    # --------------------- setters ------------------------ #
-    @training_data.setter
-    def training_data(self,data_subset):
-        'changes the value of the training_data property'
-        self._training_data = data_subset
+    def data(self):
+        return self._data
 
-    @test_data.setter
-    def test_data(self,data_subset):
-        'changes the value of the test_data property'
-        self._test_data = data_subset
+
 
