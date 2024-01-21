2e5534130290419e11817127f3aa3f61f5c5f127
Vanessa Madu <vm2218@ic.ac.uk>
2024-01-21 15:58:36 +0000

    simplify sbr

--- drifter_velocity_models/benchmark_models.py
+++ drifter_velocity_models/benchmark_models.py
@@ -18,18 +18,15 @@ class BathtubModel(Model):
 class SBRModel(Model):
     '''benchmark model: predicts velocities according to a steady solid body rotation model.'''
 
-    def __init__(self,loss_type:str,uncertainty_type:str,training_data,test_data):
-        super().__init__(loss_type,uncertainty_type,training_data,test_data)
+    def __init__(self,data):
+        super().__init__(data)
         self.f0 = 7.27e-5 # coriolis parameter at 30 degrees N
-        self.model_type = 'sbr'
 
-    #------------------------ model constructions -------------------------#
     @staticmethod
     def sbr(lon:float,lat:float,f0:float):
         __class__.check_coordinates(lon,lat)
-        
         return np.array([-f0*lat,f0*lon])
-    # -------------------------properties and setters ------------------------------#
+
     @property
     def f0(self):
         return self._f0
@@ -42,18 +39,10 @@ class SBRModel(Model):
             raise ValueError("coriolis parameter, f0, must be a real number")
         self._f0 = val
 
-    # -----------'immutable' properties -----------#
     @property
-    def model_function(self):
-        return self.sbr
+    def predictions(self):
+        return [__class__.sbr(lon,lat,self.f0) for lon,lat in np.array(self.data[["lon","lat"]])]
     
-    @property
-    def trained_prediction(self):
-        return [self.model_function(lon,lat,self.f0) for lon,lat in np.array(self.training_data[["lon","lat"]])]
-    
-    @property
-    def testing_prediction(self):
-        return [self.model_function(lon,lat,self.f0) for lon,lat in np.array(self.test_data[["lon","lat"]])]
 
 class FixedCurrentModel(Model):
     '''benchmark model: predicts all drifter velocities to be the average velocity across the 
